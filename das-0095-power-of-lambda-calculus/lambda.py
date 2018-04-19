1d8b9a7bc9c271c866dcda0104c86ff4c93a6efb
Gary Bernhardt <gary.bernhardt@gmail.com>
2018-04-19 11:40:55 -0700

    add lambda calculus code from das 0095

---
+++ das-0095-power-of-lambda-calculus/lambda.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+print(
+    ( lambda myself: ( lambda n: ( ( lambda cond: lambda t: lambda f: cond(t)(f))( ( lambda n: n(lambda x: ( lambda t: lambda f: f(( lambda x: x))))(( lambda t: lambda f: t(( lambda x: x)))))(n))( lambda _:( ( lambda n: lambda f: lambda x: f( n(f)(x) ))(( lambda f: lambda x: x))) )( lambda _: ( lambda n: lambda m: n(lambda x: ( lambda n: lambda m: n(( lambda n: lambda f: lambda x: f( n(f)(x) )))(m))(x)(m))(( lambda f: lambda x: x)))(n)( myself(myself)(( (lambda n: lambda f: lambda x: n (lambda g: lambda h: h(g(f))) (lambda u: x) (lambda u: u)))(n)) )))))( lambda myself: ( lambda n: ( ( lambda cond: lambda t: lambda f: cond(t)(f))( ( lambda n: n(lambda x: ( lambda t: lambda f: f(( lambda x: x))))(( lambda t: lambda f: t(( lambda x: x)))))(n))( lambda _:( ( lambda n: lambda f: lambda x: f( n(f)(x) ))(( lambda f: lambda x: x))) )( lambda _: ( lambda n: lambda m: n(lambda x: ( lambda n: lambda m: n(( lambda n: lambda f: lambda x: f( n(f)(x) )))(m))(x)(m))(( lambda f: lambda x: x)))(n)( myself(myself)(( (lambda n: lambda f: lambda x: n (lambda g: lambda h: h(g(f))) (lambda u: x) (lambda u: u)))(n)) )))))
+
+    (( ( lambda n: lambda f: lambda x: f( n(f)(x) ))(( lambda n: lambda f: lambda x: f( n(f)(x) ))(( lambda n: lambda f: lambda x: f( n(f)(x) ))(( lambda n: lambda f: lambda x: f( n(f)(x) ))(( lambda n: lambda f: lambda x: f( n(f)(x) ))(( lambda n: lambda f: lambda x: f( n(f)(x) ))(( lambda f: lambda x: x)))))))))
+
+    (lambda x: x + 1)(0)
+)
